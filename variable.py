adae8a6dc084456f410e4bbc790a0f401528cd5d
Jaewoong Lee <230@fumire.moe>
2018-08-18 12:29:55 +0900

    add name

--- variable.py
+++ variable.py
@@ -1,3 +1,5 @@
+import random
+
 palette = { 
         "white":(255,255,255), 
         "black":(0,0,0), 
@@ -15,6 +17,13 @@ palette = {
         "gray":(128,128,128) 
         }
 
+playerName = ["Dale", "Leo", "Leon", "Leona", "Napoleon", "Rose", "Rosaria", "Lily", "Violet", "Maxwell", "Victor", "Victoria", "Ryan", "Bianca", "Misty", "Grace", "Margaret", "Benjamin", "Sophia", "Stella", "Amanda", "Gemma", "Clara", "Stallone", "Herminone", "Mature", "Jason", "Montgomery", "Cassiopeia", "Thatcher", "Smith", "Olivia", "Helen", "Hannah", "John", "Jane", "Julie", "Samantha", "Sarah"]
+
+def chooseName(number=1):
+    random.shuffle(playerName)
+    return playerName[:number]
+
 if __name__ == "__main__":
     for key in sorted(palette.keys()):
         print(key, palette[key])
+    print(chooseName(number=5))
